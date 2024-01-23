1c30d13d895507cc410fdf358bc8be53d55cb1d1
junghoon1039 <junghoon1039@gmail.com>
2024-01-23 15:58:22 +0900

    proxy

---
+++ openvidu-react/src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://i10a601.p.ssafy.io:8050',
+      pathRewrite: {
+        '^/api': '',
+      },
+    }),
+  );
+};
