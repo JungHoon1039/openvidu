adfedc9a83341798fddcd87cee3d284c091e409b
junghoon1039 <junghoon1039@gmail.com>
2024-01-23 17:24:57 +0900

    px

--- openvidu-react/src/setupProxy.js
+++ openvidu-react/src/setupProxy.js
@@ -1,13 +1,12 @@
-import { createProxyMiddleware } from "http-proxy-middleware";
+import createProxyMiddleware from 'http-proxy-middleware'
 
-export default (app) => {
-  app.use(
-    createProxyMiddleware('/openvidu',{
-      target: "https://i10a601.p.ssafy.io:8050",
-      changeOrigin: true,
-      pathRewrite: {
-        '^/openvidu': '' 
+export default app => {
+  app.use('/openvidu',
+  createProxyMiddleware(      
+      {
+        target: 'https://i10a601.p.ssafy.io:8050',
+        changeOrigin: true
       }
-    })
-  );
-};
+    )
+  )
+}
