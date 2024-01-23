9aadf8e016af4bc37cac3eef1f5b3ae213de4ae0
junghoon1039 <junghoon1039@gmail.com>
2024-01-23 17:29:51 +0900

    px

--- openvidu-react/src/setupProxy.js
+++ openvidu-react/src/setupProxy.js
@@ -1,8 +1,9 @@
 import createProxyMiddleware from 'http-proxy-middleware'
 
 export default app => {
-  app.use('/openvidu',
-  createProxyMiddleware(      
+  app.use(
+  createProxyMiddleware(    
+      '/openvidu',  
       {
         target: 'https://i10a601.p.ssafy.io:8050',
         changeOrigin: true,
