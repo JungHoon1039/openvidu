43feeaae4fb30acfa640e926751eb497e1c5b05c
junghoon1039 <junghoon1039@gmail.com>
2024-01-24 11:04:36 +0900

    T

--- openvidu-basic-java/src/main/java/io/openvidu/basic/java/Controller.java
+++ openvidu-basic-java/src/main/java/io/openvidu/basic/java/Controller.java
@@ -45,6 +45,7 @@ public class Controller {
 	@PostMapping("/api/sessions")
 	public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params)
 			throws OpenViduJavaClientException, OpenViduHttpException {
+		System.out.println("hello hello hello");
 		SessionProperties properties = SessionProperties.fromJson(params).build();
 		Session session = openvidu.createSession(properties);
 		return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
